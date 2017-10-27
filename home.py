f289dafb79de26468386d69ce1bf0671a039c4a9
Pasha School <pasha.school888@gmail.com>
2017-10-27 16:03:38 +0300

    add cookies for better UX

--- home.py
+++ home.py
@@ -4,6 +4,8 @@ from flask import render_template
 from flask import request
 import json
 import urllib
+import datetime
+from flask import make_response
 
 
 app = Flask(__name__)
@@ -23,37 +25,42 @@ CURRENCY_URL ="https://openexchangerates.org//api/latest.json?app_id=47131325c99
 @app.route("/", methods=["GET", "POST"])
 def home():
 	# get customized headlines, based on user input or default
-	publication = request.form.get('publication')
-	if not publication:
-		publication = DEFAULTS['publication']
+	publication = get_value_with_fallback('publication')
 
 	articles = get_news(publication)
 	# get customized weather based on user input or default
-	city = request.args.get('city')
-	if not city:
-		city = DEFAULTS['city']
+	city = get_value_with_fallback('city')
 	weather = get_wether(city)
 
-	currency_from = request.args.get("currency_from")
+	currency_from = get_value_with_fallback("currency_from")
+	currency_to = get_value_with_fallback('currency_to')
 
-	if not currency_from:
-		currency_from = DEFAULTS["currency_from"]
-
-	currency_to = request.args.get('currency_to')
-	if not currency_to:
-		currency_to = DEFAULTS['currency_to']
 	rate, currencies = get_rate(currency_from, currency_to)
 
-	# rate = get_rate(currency_from, currency_to)
+	response = make_response(render_template("home.html",
+			articles=articles,
+			weather=weather,
+			currency_from=currency_from,
+			currency_to=currency_to,
+			rate=rate,
+			currencies=sorted(currencies)))
 
+	expires = datetime.datetime.now() + datetime.timedelta(days=365)
 
-	return render_template("home.html",
-		articles=articles,
-		weather=weather,
-		currency_from=currency_from,
-		currency_to=currency_to,
-		rate=rate,
-		currencies=sorted(currencies))
+	response.set_cookie("publication", publication, expires=expires)
+	response.set_cookie("city", city, expires=expires)
+	response.set_cookie("currency_from", currency_from, expires=expires)
+	response.set_cookie("currency_to", currency_to, expires=currency_to)
+
+	return response
+
+def get_value_with_fallback(key):
+	if request.args.get(key):
+		return request.args.get(key)
+	if request.cookies.get(key):
+		return request.cookies.get(key)
+	return DEFAULTS[key]
+
 
 
 def get_rate(frm, to):
@@ -95,6 +102,8 @@ def get_wether(query):
 		}
 
 	return weather
+
+
 
 if __name__ == '__main__':
 	app.run(port=5001, debug=True)
